92ece1bd3016f2e82064bd5716627a8d06348887
Loveleen Kaur <loveleen.nancy@gmail.com>
2019-06-06 11:37:26 +0530

    Initial commit

---
+++ app/src/main/java/kitkatstudio/com/usernavigation/MainActivity.java
@@ -0,0 +1,27 @@
+package kitkatstudio.com.usernavigation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView click;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = findViewById(R.id.click);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
